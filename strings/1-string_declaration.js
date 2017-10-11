72ec062c129db6d3f1a360d8973c9f2705c6aca1
santoshtechie9 <santoshtechie9@gmail.com>
2017-10-11 09:05:56 -0700

    js examples

---
+++ strings/1-string_declaration.js
@@ -0,0 +1,37 @@
+
+
+// declare two string varaiables in java script
+var text1 = "It's alright";
+var text2 = "John Doe";
+
+//prints the variables on console 
+console.log("text1 = " + text1);
+console.log("text2 = " + text2);
+
+
+// declare two string varaiables with double quotes in it
+var text3 = "\It's alright\", said John";
+console.log("text3 = " + text3);
+
+// "\"" - backslash or "+" - plus for line break
+var text4 = "Companies including Alibaba, Lexus, and Uber use both human and automated \
+customer service to deliver the best results, according to a report from MIT Technology Review \
+and Genesys.";
+console.log("text4 = " + text4);
+
+
+/*
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo"></p> 
+
+<script>
+var txt = "\"Hello World!\"";
+document.getElementById("demo").innerHTML = txt;
+</script>
+
+</body>
+</html>
+*/
---
+++ strings/2-string_length.js
@@ -0,0 +1,24 @@
+
+
+
+var txt = "Hello World!";
+
+console.log("txt.length = " + txt.length );
+
+
+
+/*
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo"></p> 
+
+<script>
+var txt = "Hello World!";
+document.getElementById("demo").innerHTML = txt.length;
+</script>
+
+</body>
+</html>
+*/
