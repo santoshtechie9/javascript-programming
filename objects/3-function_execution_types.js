7515826bf938ce3e5d848b573cd1eafece655dba
Santosh <santoshtechie9@gmail.com>
2018-07-22 09:12:56 -0700

    Update 3-function_execution_types.js

--- objects/3-function_execution_types.js
+++ objects/3-function_execution_types.js
@@ -1,5 +1,6 @@
 
 //The following example will show the different ways of executing functions in javascript
+// The this keyword points to different objects depending on the context. it can refer to a window(browser), global object(node js), current object(object.foo()), empty object(new function())
 
 
 //Method #1 : in this method of calling function this refers to the global object.
@@ -7,18 +8,15 @@ function foo() {
     console.log("Hello, this inside foo function");
     console.log(this);
 }
-
 foo();    
 
 
 //Method#2 : in this method of calling function this refer to the current object
-
 var object = {};
 object.foo = function(){
     console.log("Hello, this is foo function inside object");
     console.log(this);
 }
-
 object.foo();   
 
  //Method#3 : in this method of calling function this refers to an empty object
